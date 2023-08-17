04bdaeac26f984914b341732a3b729055dc053da
Edmund Ju <edmundju@berkeley.edu>
2023-08-17 16:19:04 -0700

    upload directory contents and print uploaded files

--- check_bucket_name.py
+++ check_bucket_name.py
@@ -1,4 +1,4 @@
-import boto3, subprocess, concurrent.futures
+import boto3, subprocess, concurrent.futures, os
 
 def is_bucket_name_available(bucket_name):
     try:
@@ -11,9 +11,35 @@ def is_bucket_name_available(bucket_name):
 def run_terraform_apply(bucket_name):
     try:
         subprocess.run(f"TF_VAR_bucket_name={bucket_name} terraform apply", shell=True, check=True)
-        print('Completed')
+        print('Bucket created.')
+
+        upload_directory_contents(bucket_name)
+
     except subprocess.CalledProcessError as e:
         print('Error running "terraform apply": ', e)
+
+def upload_directory_contents(bucket_name):
+    s3 = boto3.client('s3')
+    source_dir = "./directory_to_upload"
+
+    for root, _, files in os.walk(source_dir):
+        for file in files:
+            local_path = os.path.join(root, file)
+            s3_key = os.path.relpath(local_path, source_dir)
+
+            s3.upload_file(local_path, bucket_name, s3_key)
+            print(f"Uploaded: {s3_key}")
+
+    # List uploaded files
+    response = s3.list_objects(Bucket=bucket_name)
+    if 'Contents' in response:
+        print("Uploaded Files: ")
+        for obj in response["Contents"]:
+            print(obj['Key'])
+
+
+
+
 
 def main():
     print("AWS S3 Bucket Name Availability Checker")
